a6ea419c3d8758268044bad7224d01a01c13eabd
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-15 17:16:09 -0300

    feat: add images and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import { Nunito } from "next/font/google"
 
+import { getCssText } from "@/styles"
+import { globalStyles } from "@/styles/global"
+
 import { config } from "./metadata"
 
 const nunito = Nunito({ subsets: ["latin"] })
 
 export const metadata = config
 
+globalStyles()
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </head>
       <body className={nunito.className}>{children}</body>
     </html>
   )
